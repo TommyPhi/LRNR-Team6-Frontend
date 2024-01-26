9eed50b6d2988ea212d4873d96c5709b1cc59962
Jessica Contreras Ayala <hoshostars@hotmail.com>
2024-01-26 14:48:43 -0500

    quiz form added

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import QuizForm from './components/QuizForm';  // Adjust the path based on your project structure
+
 
 export default function Quiz() {
   return (
     <div>
+      <br></br>
       <h1>Quiz Page</h1>
+      <br></br>
+      <QuizForm />
+
     </div>
   )
 }
